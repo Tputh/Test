597b1cb1de7fdbd259bd29f7b3cb9a30b6a6eb83
Имя <vitya.klyucherov@mail.ru>
2023-03-27 19:07:02 +0300

    Дз. Тесты

--- src/main/java/com/klyucherov/test/User.java
+++ src/main/java/com/klyucherov/test/User.java
@@ -1,4 +1,26 @@
 package com.klyucherov.test;
 
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import java.util.*;
+@Data
+@RequiredArgsConstructor
 public class User {
+    private String login;
+    private String email;
+
+    public User(String login, String email) {
+        checkEmail(email);
+        this.login = login;
+        this.email = email;
+    }
+
+    public boolean checkEmail(String email) {
+        if (!email.contains("@") || !email.contains(".")){
+            throw new RuntimeException("в ведены не коректные данные");
+        }
+        return true;
+    }
 }
+
--- src/test/java/com/klyucherov/test/UserTest.java
+++ src/test/java/com/klyucherov/test/UserTest.java
@@ -1,4 +1,42 @@
+
+
 package com.klyucherov.test;
 
-public class UserTest {
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class UserTest {
+    private User user;
+
+
+
+
+    @Test
+    public void testingConstructor() {
+        user = new User("test","@.");
+        Assertions.assertEquals(user.getEmail(), "@.");
+        Assertions.assertEquals(user.getLogin(), "test");
+    }
+
+    @Test
+    public void testingNulParameters() {
+        user = new User();
+        Assertions.assertNull(user.getLogin());
+        Assertions.assertNull(user.getEmail());
+    }
+
+    @Test
+    public void testingEmail() {
+        Assertions.assertThrows(RuntimeException.class, () -> user.checkEmail("test"));
+    }
+
+    @Test
+    public void testingСompare() {
+        user = new User("test","@.");
+        Assertions.assertNotEquals(user.getEmail(), user.getLogin());
+    }
+
+
 }
